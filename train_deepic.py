1ccc74257193211bd6bffb36bb1e1e736cc77f80
Yangyang Hu <yhu017@ucr.edu>
2020-07-12 13:50:06 -0700

    add train

--- train_deepic.py
+++ train_deepic.py
@@ -14,46 +14,33 @@ from models.loss import GeneratorLoss
 from models.ssim import ssim
 from math import log10
 
-from all_parser import root_dir
-
-cs = np.column_stack
-
+# from all_parser import root_dir
 
 # data_dir: directory storing processed data
-data_dir = os.path.join(root_dir, 'data')
+# data_dir = os.path.join(root_dir, 'data')
 
 # out_dir: directory storing checkpoint files
-out_dir = os.path.join(root_dir, 'checkpoints')
-os.makedirs(out_dir, exist_ok=True)
+# out_dir = os.path.join(root_dir, 'checkpoints')
 
-datestr = time.strftime('%m_%d_%H_%M')
-visdom_str=time.strftime('%m%d')
 
-"""resos = '10kb40kb'
-chunk = 40
-stride = 40
-bound = 201
-pool = 'nonpool'
-
-upscale = 1
-num_epochs = 200
-batch_size = 64"""
-
-def train(lr=40000, 
-            hr=10000,
-            chunk = 40,
-            stride = 40,
-            bound = 201,
-            pool = 'nonpool',
-            upscale = 1,
-            num_epochs = 200,
-            batch_size = 64):
+def train(data_dir, out_dir, lr=40000, hr=10000,
+          chunk=40, stride=40, bound=201, pool='nonpool',
+          upscale=1, num_epochs=200, batch_size=64):
+    """ data_dir: directory storing processed data
+        out_dir: directory storing checkpoint files"""
     resos = str(hr)+str(lr)
+    cs = np.column_stack
+    os.makedirs(out_dir, exist_ok=True)
+
+    datestr = time.strftime('%m_%d_%H_%M')
+    visdom_str = time.strftime('%m%d')
+
     # whether using GPU for training
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
     # prepare training dataset
-    train_file = os.path.join(data_dir, f'deephic_{resos}_c{chunk}_s{stride}_b{bound}_{pool}_train.npz')
+    train_file = os.path.join(
+        data_dir, f'deephic_{resos}_c{chunk}_s{stride}_b{bound}_{pool}_train.npz')
     train = np.load(train_file)
 
     train_data = torch.tensor(train['data'], dtype=torch.float)
@@ -63,7 +50,8 @@ def train(lr=40000,
     train_set = TensorDataset(train_data, train_target, train_inds)
 
     # prepare valid dataset
-    valid_file = os.path.join(data_dir, f'deephic_{resos}_c{chunk}_s{stride}_b{bound}_{pool}_valid.npz')
+    valid_file = os.path.join(
+        data_dir, f'deephic_{resos}_c{chunk}_s{stride}_b{bound}_{pool}_valid.npz')
     valid = np.load(valid_file)
 
     valid_data = torch.tensor(valid['data'], dtype=torch.float)
@@ -73,8 +61,10 @@ def train(lr=40000,
     valid_set = TensorDataset(valid_data, valid_target, valid_inds)
 
     # DataLoader for batched training
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, drop_last=True)
-    valid_loader = DataLoader(valid_set, batch_size=batch_size, shuffle=False, drop_last=True)
+    train_loader = DataLoader(
+        train_set, batch_size=batch_size, shuffle=True, drop_last=True)
+    valid_loader = DataLoader(
+        valid_set, batch_size=batch_size, shuffle=False, drop_last=True)
 
     # load network
     netG = Generator(upscale, in_channel=1, resblock_num=5).to(device)
@@ -92,7 +82,8 @@ def train(lr=40000,
 
     best_ssim = 0
     for epoch in range(1, num_epochs+1):
-        run_result = {'nsamples': 0, 'd_loss': 0, 'g_loss': 0, 'd_score': 0, 'g_score': 0}
+        run_result = {'nsamples': 0, 'd_loss': 0,
+                      'g_loss': 0, 'd_score': 0, 'g_score': 0}
 
         netG.train()
         netD.train()
@@ -128,13 +119,14 @@ def train(lr=40000,
             run_result['d_score'] += real_out.mean().item() * batch_size
             run_result['g_score'] += fake_out.mean().item() * batch_size
 
-            train_bar.set_description(desc=f"[{epoch}/{num_epochs}] Loss_D: {run_result['d_loss']/run_result['nsamples']:.4f} Loss_G: {run_result['g_loss']/run_result['nsamples']:.4f} D(x): {run_result['d_score']/run_result['nsamples']:.4f} D(G(z)): {run_result['g_score']/run_result['nsamples']:.4f}")
+            train_bar.set_description(
+                desc=f"[{epoch}/{num_epochs}] Loss_D: {run_result['d_loss']/run_result['nsamples']:.4f} Loss_G: {run_result['g_loss']/run_result['nsamples']:.4f} D(x): {run_result['d_score']/run_result['nsamples']:.4f} D(G(z)): {run_result['g_score']/run_result['nsamples']:.4f}")
         train_gloss = run_result['g_loss']/run_result['nsamples']
         train_dloss = run_result['d_loss']/run_result['nsamples']
         train_dscore = run_result['d_score']/run_result['nsamples']
         train_gscore = run_result['g_score']/run_result['nsamples']
 
-        valid_result = {'g_loss': 0, 'd_loss': 0, 'g_score': 0, 'd_score': 0, 
+        valid_result = {'g_loss': 0, 'd_loss': 0, 'g_score': 0, 'd_score': 0,
                         'mse': 0, 'ssims': 0, 'psnr': 0, 'ssim': 0, 'nsamples': 0}
         netG.eval()
         netD.eval()
@@ -153,7 +145,7 @@ def train(lr=40000,
                 d_loss_fake = criterionD(sr_out, torch.zeros_like(sr_out))
                 d_loss = d_loss_real + d_loss_fake
                 g_loss = criterionG(sr_out.mean(), sr, hr)
-                
+
                 valid_result['g_loss'] += g_loss.item() * batch_size
                 valid_result['d_loss'] += d_loss.item() * batch_size
                 valid_result['g_score'] += sr_out.mean().item() * batch_size
@@ -163,37 +155,49 @@ def train(lr=40000,
                 valid_result['mse'] += batch_mse * batch_size
                 batch_ssim = ssim(sr, hr)
                 valid_result['ssims'] += batch_ssim * batch_size
-                valid_result['psnr'] = 10 * log10(1/(valid_result['mse']/valid_result['nsamples']))
-                valid_result['ssim'] = valid_result['ssims'] / valid_result['nsamples']
-                valid_bar.set_description(desc=f"[Predicting in Test set] PSNR: {valid_result['psnr']:.4f} dB SSIM: {valid_result['ssim']:.4f}")
+                valid_result['psnr'] = 10 * \
+                    log10(1/(valid_result['mse']/valid_result['nsamples']))
+                valid_result['ssim'] = valid_result['ssims'] / \
+                    valid_result['nsamples']
+                valid_bar.set_description(
+                    desc=f"[Predicting in Test set] PSNR: {valid_result['psnr']:.4f} dB SSIM: {valid_result['ssim']:.4f}")
 
         valid_gloss = valid_result['g_loss'] / valid_result['nsamples']
         valid_dloss = valid_result['d_loss'] / valid_result['nsamples']
         valid_gscore = valid_result['g_score'] / valid_result['nsamples']
         valid_dscore = valid_result['d_score'] / valid_result['nsamples']
         now_ssim = valid_result['ssim'].item()
-        
+
         if epoch == 1:
-            vis_dloss = vis.line(X=cs((epoch, epoch)), Y=cs((train_dloss, valid_dloss)), opts=dict(title='Discriminator Loss', legend=['Train', 'Test']))
-            vis_gloss = vis.line(X=cs((epoch, epoch)), Y=cs((train_gloss, valid_gloss)), opts=dict(title='Generator Loss', legend=['Train', 'Test']))
-            vis_dscore = vis.line(X=cs((epoch, epoch)), Y=cs((train_dscore, valid_dscore)), opts=dict(title='Discriminator Score of true images', legend=['Train', 'Test']))
-            vis_gscore = vis.line(X=cs((epoch, epoch)), Y=cs((train_gscore, valid_gscore)), opts=dict(title='Generator Score of fake images', legend=['Train', 'Test']))
-            vis_ssim = vis.line([now_ssim], X=[epoch], opts=dict(title='SSIM scores in test dataset'))
+            vis_dloss = vis.line(X=cs((epoch, epoch)), Y=cs((train_dloss, valid_dloss)), opts=dict(
+                title='Discriminator Loss', legend=['Train', 'Test']))
+            vis_gloss = vis.line(X=cs((epoch, epoch)), Y=cs((train_gloss, valid_gloss)), opts=dict(
+                title='Generator Loss', legend=['Train', 'Test']))
+            vis_dscore = vis.line(X=cs((epoch, epoch)), Y=cs((train_dscore, valid_dscore)), opts=dict(
+                title='Discriminator Score of true images', legend=['Train', 'Test']))
+            vis_gscore = vis.line(X=cs((epoch, epoch)), Y=cs((train_gscore, valid_gscore)), opts=dict(
+                title='Generator Score of fake images', legend=['Train', 'Test']))
+            vis_ssim = vis.line([now_ssim], X=[epoch], opts=dict(
+                title='SSIM scores in test dataset'))
         else:
-            vis.line(X=cs((epoch, epoch)), Y=cs((train_dloss, valid_dloss)), update='append', win=vis_dloss, opts=dict(legend=['Train', 'Test']))
-            vis.line(X=cs((epoch, epoch)), Y=cs((train_gloss, valid_gloss)), update='append', win=vis_gloss, opts=dict(legend=['Train', 'Test']))
-            vis.line(X=cs((epoch, epoch)), Y=cs((train_dscore, valid_dscore)), update='append', win=vis_dscore, opts=dict(legend=['Train', 'Test']))
-            vis.line(X=cs((epoch, epoch)), Y=cs((train_gscore, valid_gscore)), update='append', win=vis_gscore, opts=dict(legend=['Train', 'Test']))
+            vis.line(X=cs((epoch, epoch)), Y=cs((train_dloss, valid_dloss)),
+                     update='append', win=vis_dloss, opts=dict(legend=['Train', 'Test']))
+            vis.line(X=cs((epoch, epoch)), Y=cs((train_gloss, valid_gloss)),
+                     update='append', win=vis_gloss, opts=dict(legend=['Train', 'Test']))
+            vis.line(X=cs((epoch, epoch)), Y=cs((train_dscore, valid_dscore)),
+                     update='append', win=vis_dscore, opts=dict(legend=['Train', 'Test']))
+            vis.line(X=cs((epoch, epoch)), Y=cs((train_gscore, valid_gscore)),
+                     update='append', win=vis_gscore, opts=dict(legend=['Train', 'Test']))
             vis.line([now_ssim], X=[epoch], update='append', win=vis_ssim)
 
         if now_ssim > best_ssim:
             best_ssim = now_ssim
             print(f'Now, Best ssim is {best_ssim:.6f}')
             best_ckpt_file = f'{datestr}_bestg_{resos}_c{chunk}_s{stride}_b{bound}_{pool}_deephic.pytorch'
-            torch.save(netG.state_dict(), os.path.join(out_dir, best_ckpt_file))
+            torch.save(netG.state_dict(), os.path.join(
+                out_dir, best_ckpt_file))
     final_ckpt_g = f'{datestr}_finalg_{resos}_c{chunk}_s{stride}_b{bound}_{pool}_deephic.pytorch'
     final_ckpt_d = f'{datestr}_finald_{resos}_c{chunk}_s{stride}_b{bound}_{pool}_deephic.pytorch'
 
     torch.save(netG.state_dict(), os.path.join(out_dir, final_ckpt_g))
     torch.save(netD.state_dict(), os.path.join(out_dir, final_ckpt_d))
-
