0fd52bbe77bc0b50a6b97484c4e26f2b538ee7ca
Yangyang Hu <yhu017@ucr.edu>
2020-10-07 14:49:09 -0700

    update

--- predict_deephic.py
+++ predict_deephic.py
@@ -55,11 +55,11 @@ def deephic_predictor(deephic_loader, ckpt_file, scale, res_num, device):
     deep_hics = together(result_data, result_inds, tag='Reconstructing: ')
     return deep_hics
 
+def save_data_n(key, deep_hics, compacts, sizes, file, low_res):
+    file = os.path.join(out_dir, f'predict_chr{key}_{low_res}.npz')
+    save_data(deep_hics[key], compacts[key], sizes[key], file)
+
 def save_data(deep_hic, compact, size, file):
-    print(file)
-    print(size)
-    print(deep_hic.shape)
-    print(compact.shape)
     deephic = spreadM(deep_hic, compact, size, convert_int=False, verbose=True)
     np.savez_compressed(file, deephic=deephic, compact=compact)
     print('Saving file:', file)
@@ -93,21 +93,14 @@ def predict(data_dir, out_dir, lr=40000, ckpt_file=None):
     
     indices, compacts, sizes = data_info(deephic_data)
     deep_hics = deephic_predictor(deephic_loader, ckpt_file, scale, res_num, device)
-    
-    """def save_data_n(key):
-        file = os.path.join(out_dir, f'predict_chr{key}_{low_res}.npz')
-        print(compacts[key].shape)
-        save_data(deep_hics[key], compacts[key], sizes[key], file)"""
 
     pool = multiprocessing.Pool(processes=pool_num)
     print(f'Start a multiprocess pool with process_num = {pool_num} for saving predicted data')
     print('Output path: ', out_dir)
     for key in compacts.keys():
-        print(key)
-        file = os.path.join(out_dir, f'predict_chr{key}_{low_res}.npz')
-        save_data(deep_hics[key], compacts[key], sizes[key], file)
-        #pool.apply_async(save_data_n, (key,))
-    #pool.close()
-    #pool.join()
+        pool.apply_async(save_data_n, (key,deep_hics, compacts, sizes, file, low_res, ))
+
+    pool.close()
+    pool.join()
     
     print(f'All data saved. Running cost is {(time.time()-start)/60:.1f} min.')
